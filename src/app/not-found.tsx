b2c1837df6b9d0814450f2d170c9b0efe9fb7434
v1cferr <dev.victorferreira@gmail.com>
2024-12-14 20:37:16 -0300

    fix: add Head component for SEO in 404 error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,15 +2,15 @@
 
 import Link from "next/link";
 import SpotifyPlayer from "@/components/SpotifyPlayer";
-
-export const metadata = {
-  title: "404 error | v1cferr",
-  description: "404 error | v1cferr",
-};
+import Head from "next/head";
 
 const NotFoundPage = () => {
   return (
     <>
+      <Head>
+        <title>404 error | v1cferr</title>
+        <meta name="description" content="404 error | v1cferr" />
+      </Head>
       <div>
         <h1>404 - Página Não Encontrada</h1>
         <p>Desculpe, a página que você está procurando não existe.</p>
