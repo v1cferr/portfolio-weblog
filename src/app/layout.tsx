1cb80609eb8ca3d661ac348459aa9e956b9238a9
v1cferr <dev.victorferreira@gmail.com>
2024-12-14 21:06:23 -0300

    refactor: update import path for global styles in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
