04fbd340ae969e647242e2d211db3bfd97ad47ca
v1cferr <dev.victorferreira@gmail.com>
2024-12-17 22:54:30 -0300

    feat: add language selector component and update localization files for language switching

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslations } from "next-intl";
+import LanguageSelector from "@/components/LanguageSelector";
 
 const About = () => {
   const t = useTranslations("About");
@@ -8,6 +9,8 @@ const About = () => {
     <>
       <h1>{t("title")}</h1>
       <p>{t("description")}</p>
+
+      <LanguageSelector />
     </>
   );
 };
---
+++ src/components/LanguageSelector.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useLocale, useTranslations } from "next-intl";
+import { routing } from "@/i18n/routing";
+import LocaleSwitcherSelect from "./LocaleSwitcherSelect";
+
+export default function LanguageSelector() {
+  const t = useTranslations("LocaleSwitcher");
+  const locale = useLocale();
+
+  return (
+    <LocaleSwitcherSelect defaultValue={locale} label={t("label")}>
+      {routing.locales.map((cur) => (
+        <option key={cur} value={cur}>
+          {t(`locale.${cur}`)}
+        </option>
+      ))}
+    </LocaleSwitcherSelect>
+  );
+}
