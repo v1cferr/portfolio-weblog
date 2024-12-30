c13f83c727a59d55840b23b73b36ee02ab304d14
v1cferr <dev.victorferreira@gmail.com>
2024-12-30 19:42:39 +0000

    fix(Loading): use self-closing tag for span element

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <>
-      <span className="loading loading-infinity loading-lg"></span>
+      <span className="loading loading-infinity loading-lg" />
     </>
   );
 };
