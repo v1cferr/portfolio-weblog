67ac7d4b942d2f6bc59c2e418acec50643dd1489
v1cferr <dev.victorferreira@gmail.com>
2025-01-18 04:57:13 +0000

    fix(not-found): update NotFoundPage content and structure

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -5,12 +5,13 @@ export default function NotFoundPage() {
   // return <h1>{t('title')}</h1>;
 
   return (
-    <>
+    <div>
       <h1>oi</h1>
+      <h2>pagina não encontrada</h2>
       <p>
-        apenas um teste do <code>not-found</code>
+        esse é apenas um teste do <code>not-found</code>
       </p>
       <p>404</p>
-    </>
+    </div>
   );
 }
