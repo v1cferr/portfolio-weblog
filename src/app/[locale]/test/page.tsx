457b682778fc7c23717079d3a7243226a0bb35d7
v1cferr <dev.victorferreira@gmail.com>
2025-01-19 20:36:28 +0000

    feat(test): add TODO for simulating login flow and rendering profile data

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -8,6 +8,20 @@ const Test = () => {
     <>
       <h1>{t("title")}</h1>
       <p>{t("description")}</p>
+
+      {/* 
+        TODO: Simular um clique no button para realizar todo o fluxo a seguir:
+        
+        1. Clicar no button para acessar a rota /api/blizzard/login
+        2. Ser redirecionado para a página de login da Blizzard (já autorizado)
+        3. Ser redirecionado para a rota /api/blizzard/token com o código de autorização
+        4. Ser redirecionado para a rota /api/blizzard/profile com o token de acesso
+        5. Acessar a rota /api/blizzard/profile e retornar um JSON com os dados
+          - https://us.api.blizzard.com/profile/user/wow
+        6. Renderizar os dados no front
+        
+        Tudo isso utilizando Serverless Functions e sem expor dados sensíveis no front.
+      */}
       <Link href="/api/blizzard/login">
         <button className="btn">Log In</button>
       </Link>
