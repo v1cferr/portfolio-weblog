21f3b9143fd0896446798efb2da0dd02c2bfd47c
v1cferr <dev.victorferreira@gmail.com>
2025-02-08 14:14:32 -0300

    refactor(Hero): improve layout and styling for better readability and alignment

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -31,18 +31,18 @@ const socialLinks = [
 
 export default function Hero() {
   return (
-    <section className="flex flex-col items-center text-center px-6 py-16 bg-base-200 text-base-content md:flex-row md:justify-center md:gap-8">
+    <section className="flex flex-col items-center text-center px-6 py-16 bg-base-200 text-base-content md:flex-row md:justify-center md:items-start md:gap-8">
       {/* Moldura para imagem futura */}
-      <div className="w-24 h-24 md:w-32 md:h-32 rounded-full border-2 border-primary flex items-center justify-center mb-4 md:mb-0">
+      <div className="w-24 h-24 md:w-32 md:h-32 rounded-full border-2 border-primary flex items-center justify-center mb-4 md:mb-0 md:mr-8">
         {/* Substituir pelo <Image src="imagem.jpg" className="rounded-full w-full h-full" /> no futuro */}
       </div>
 
       {/* Conteúdo centralizado */}
-      <div className="flex flex-col items-center text-center">
-        <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl">
+      <div className="flex flex-col items-center text-center md:items-start md:text-left gap-y-2">
+        <h1 className="text-3xl font-bold sm:text-3xl md:text-4xl">
           Oi, sou o Victor.
         </h1>
-        <p className="text-lg sm:text-xl md:text-2xl font-semibold text-primary">
+        <p className="text-xl md:text-2xl font-semibold text-primary">
           Software-AI Engineer in Progress
         </p>
 
@@ -58,10 +58,18 @@ export default function Hero() {
           ))}
         </div>
 
-        {/* Quote */}
+        {/* Quotes */}
+        {/* TODO: Estilizar como os quotes do HackMD */}
+
+        {/* https://kultivi.com/blog/idiomas/dez-proverbios-chineses-e-seus-significados */}
         <blockquote className="mt-4 text-sm italic text-base-content/70 max-w-sm">
           &quot;Não tenha medo de crescer lentamente, tenha medo de ficar
           parado.&quot;
+        </blockquote>
+
+        {/* https://en.m.wikipedia.org/wiki/A_journey_of_a_thousand_miles_begins_with_a_single_step */}
+        <blockquote className="mt-2 text-sm italic text-base-content/70 max-w-sm">
+          &quot;Uma jornada de mil milhas começa com um único passo.&quot;
         </blockquote>
       </div>
     </section>
