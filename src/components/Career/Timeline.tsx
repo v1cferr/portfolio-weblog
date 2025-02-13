f988074162fa9fd0055459fea058b03926094032
v1cferr <dev.victorferreira@gmail.com>
2025-02-12 21:28:16 -0300

    refactor(Career/Timeline): add aria-hidden attribute to TimelineIcon and refactor TimelineItemComponent props

--- src/components/Career/Timeline.tsx
+++ src/components/Career/Timeline.tsx
@@ -47,7 +47,8 @@ const TimelineIcon: React.FC = () => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
       fill="currentColor"
-      className="h-5 w-5">
+      className="h-5 w-5"
+      aria-hidden="true">
       <path
         fillRule="evenodd"
         d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
@@ -57,11 +58,17 @@ const TimelineIcon: React.FC = () => {
   );
 };
 
-const TimelineItemComponent: React.FC<{
+interface TimelineItemComponentProps {
   item: TimelineItem;
   index: number;
   isLast: boolean;
-}> = ({ item, index, isLast }) => {
+}
+
+const TimelineItemComponent: React.FC<TimelineItemComponentProps> = ({
+  item,
+  index,
+  isLast,
+}) => {
   return (
     <li>
       {index !== 0 && <hr />}
