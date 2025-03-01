f4e6f4c3450ac49e5b1289b3dd5afb3f3e346e97
v1cferr <dev.victorferreira@gmail.com>
2025-03-01 17:45:40 -0300

    feat(Setup): modularize setup data into separate utils file

---
+++ src/utils/setupData.ts
@@ -0,0 +1,184 @@
+export const components = [
+  {
+    category: "Componentes",
+    items: [
+      {
+        name: "Processador (CPU)",
+        description:
+          "Intel Core i5-11400 11ª Geração, Cache 12MB, 2.6 GHz (4.4GHz Turbo), LGA1200",
+      },
+      {
+        name: "Placa-mãe (MOBO)",
+        description: "Asus EX-B560M-V5, Intel B560 LGA 1200, DDR4, ATX",
+      },
+      {
+        name: "Placa de vídeo (GPU)",
+        description:
+          "Gigabyte NVIDIA GeForce RTX 3050 Gaming OC, LHR, 8GB, GDDR6, DLSS, Ray Tracing",
+      },
+      {
+        name: "Memória (RAM)",
+        description:
+          "(2x8 GB) TEAM GROUP T-FORCE VULCAN PICHAU, 8GB (1X8), DDR4, 3200MHZ, C16, VERMELHA",
+      },
+      {
+        name: "Armazenamento (M2 Gen 4)",
+        description:
+          "SSD 1TB Kingston KC3000, M.2 2280 PCIe, NVMe, Leitura: 7000MB/s e Gravação: 6000MB/s",
+      },
+      {
+        name: "Armazenamento (M2 Gen 3)",
+        description:
+          "KingSpec 1TB M.2 NMVe 2280 PCIe 3x4 Leitura: 2400Mbps, Escrita: 1800Mbps",
+        url: "https://pt.aliexpress.com/item/32847169083.html",
+      },
+      {
+        name: "Fonte de alimentação (PSU)",
+        description: "Sharkoon WPM Gold Zero, 550W, 80 Plus Gold, Semi-Modular",
+      },
+      {
+        name: "Gabinete (CASE)",
+        description:
+          "Gabinete Gamer Sharkoon VS9 RGB, Mid Tower, ATX, Lateral em Vidro Temperado, 3x Cooler Fan, Branco - VS9 RGB",
+        url: "https://www.kabum.com.br/produto/508876/gabinete-gamer-sharkoon-vs9-rgb-mid-tower-atx-lateral-em-vidro-temperado-3x-cooler-fan-branco-vs9-rgb",
+      },
+    ],
+  },
+  {
+    category: "Periféricos",
+    items: [
+      {
+        name: "Water Cooler",
+        description: "Pichau Gaming Aqua X100 RTB RGB, PG-AQX100-RTB",
+      },
+      {
+        name: "Adaptador de Rede",
+        description:
+          "Adaptador WiFi D-Link, AC1200, Dual-Band, 5GHz 802.11ac, USB 3.0 - DWA-182",
+      },
+      {
+        name: "Caixa de Som",
+        description:
+          "Soundbar Gamer Redragon Adiemus, 6W RMS, RGB, USB, 150Hz/20KHz, Botão Touch, Preto - GS560",
+      },
+      {
+        name: "Cadeira",
+        description: "Cadeira Gamer Mymax Mx5, Giratória, Preto/Vermelho",
+      },
+      {
+        name: "Mouse",
+        description:
+          "Razer DeathAdder V2 Gaming Mouse 20000 DPI Sensor óptico-Iluminação Chroma RGB-8 botões programáveis",
+      },
+      {
+        name: "Controle Joystick",
+        description:
+          "Gamepad Machenike G5 Pro Elite Hall Gatilho Joystick Botões Mecha-Táteis Para Switch PC Android IOS",
+      },
+      {
+        name: "Teclado Mecânico",
+        description:
+          "Redragon TECLADO OPTICO GAMER MAGIC-WAND PRO RGB BRANCO SWITCH VERMELHO ABNT2",
+        url: "https://www.redragon.com.br/magicwandpro-lunarwhite",
+      },
+      {
+        name: "Webcam",
+        description: "Webcam Redragon Streaming Fobos, 720p | KaBuM!",
+      },
+      {
+        name: "Wireless Headset over-ear",
+        description:
+          "Baseus GH02 Gaming Wireless Headphone com microfone Over-Ear Fones de ouvido Bluetooth 5.3 40mm Driver 2.4G/Wireless/Cable RGB",
+        url: "https://pt.aliexpress.com/item/1005005875379717.html",
+      },
+      {
+        name: "Fone in-ear",
+        description: "Baseus Bowie MA10 Pro ANC Earphones",
+        url: "https://pt.aliexpress.com/item/1005006079931969.html",
+      },
+      {
+        name: "Monitor principal",
+        description:
+          "Monitor Gamer LG UltraGear 27 Full HD, 144Hz, 1ms, IPS, HDMI e DisplayPort, HDR 10, 99% sRGB, FreeSync Premium, VESA - 27GN65R",
+        url: "https://www.lg.com/br/monitores/monitores-ultragear/27gn65r-b/",
+      },
+      {
+        name: "Monitor secundário",
+        description: 'Monitor AOC 21.5" 22B1H Led Full HD',
+      },
+    ],
+  },
+  {
+    category: "Possíveis Upgrades",
+    items: [
+      {
+        name: "Nobreak Senoidal",
+        description: "ATTIV SENO 700 VA BI",
+        url: "https://www.intelbras.com/pt-br/energia/nobreaks/senoidal",
+      },
+      {
+        name: "Soundbar com Subwoofer",
+        description:
+          "Caixa de som com Subwoofer 30W RMS EDIFIER G1500 MAX - Preto",
+        url: "https://edifier.com.br/caixa-de-som-gamer-bluetooth-edifier-g1500-max-hecate-preto.html",
+      },
+    ],
+  },
+  {
+    // TODO: Completar a seção de ex-peças (e.g.: Redragon Kumara, Razer Hammerhead, Sharkoon VG6, etc.)
+    // TODO: Posteriormente adicionar um review para cada ex-peça
+    category: "Ex-Peças",
+    items: [
+      {
+        name: "Teclado Mecânico",
+        description: "Redragon Kumara Switch Blue ABNT2 K552",
+        url: "https://www.redragon.com.br/kumarargbabnt2blue",
+      },
+      {
+        name: "Fone de Ouvido",
+        description: "Razer Hammerhead",
+        url: "https://www.razer.com/br-pt/mobile-earphones/razer-hammerhead-true-wireless-x",
+      },
+      {
+        name: "Gabinete",
+        description: "Sharkoon VG6",
+        url: "https://pt.sharkoon.com/product/VG6W",
+      },
+      {
+        name: "Mouse",
+        description: "Redragon Cobra M711",
+        url: "https://www.redragon.com.br/cobra",
+        // date: "2021-06-01",
+        // time: "3 meses", (TODO: Fazer uma lógica para calcular o tempo)
+      },
+      {
+        name: "Headset over-ear",
+        description: "Redragon Zeus 7.1 H510",
+        url: "https://www.redragon.com.br/zeus",
+      },
+    ],
+  },
+];
+
+export const stores = [
+  {
+    name: "Kabum",
+    url: "https://www.kabum.com.br",
+  },
+  {
+    name: "AliExpress",
+    url: "https://pt.aliexpress.com",
+  },
+  {
+    name: "Redragon",
+    url: "https://www.redragon.com.br",
+  },
+  {
+    name: "TerabyteShop",
+    url: "https://www.terabyteshop.com.br",
+  },
+  {
+    name: "Pichau",
+    url: "https://www.pichau.com.br",
+  },
+];
